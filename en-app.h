79ca9d91ab6a414be61dad8b95a04bf1098a7d9d
enburk <enburk@gmail.com>
2024-06-12 11:34:09 +0300

    sort & shuffle

--- en-app.h
+++ en-app.h
@@ -31,6 +31,8 @@ widget<App>
     gui::button fast;
     gui::button speed;
     gui::button mute;
+    gui::button sort;
+    gui::button shuff;
 
     gui::console report;
     gui::console errors;
@@ -67,6 +69,8 @@ widget<App>
         fast .text.text = "faster";
         mode .text.text = "playing";
         mute .text.text = "mute";
+        sort .text.text = "sort";
+        shuff.text.text = "shuffle";
         slow.repeat_delay = 0ms;
         fast.repeat_delay = 0ms;
         slow.repeat_lapse = 16ms;
@@ -81,6 +85,12 @@ widget<App>
         Twos.reload("app::Twos", app::two::course.root);
     }
 
+    void one_reload () 
+    {
+        ones.reload("app::ones", app::one::course.root);
+        Ones.reload("app::Ones", app::one::course.root);
+        one.reload();
+    }
     void reload () try
     {
         ones.reload("app::ones", app::one::course.root);
@@ -109,22 +119,24 @@ widget<App>
         int l = splitter1.set("app::splitter1",  7,  9, 11);
         int r = splitter2.set("app::splitter2", 50, 60, 70);
 
-        mode  .coord = xywh(l+0*w+0*v, H-h+d, 2*v, h-d-d);
-        slow  .coord = xywh(l+0*w+2*v, H-h+d, 2*v, h-d-d);
-        speed .coord = xywh(l+0*w+4*v, H-h+d, 1*v, h-d-d);
-        fast  .coord = xywh(l+0*w+5*v, H-h+d, 2*v, h-d-d);
-        mute  .coord = xywh(l+0*w+7*v, H-h+d, 2*v, h-d-d);
+        mode  .coord = xywh(l+0*w+ 0*v, H-h+d, 2*v, h-d-d);
+        slow  .coord = xywh(l+0*w+ 2*v, H-h+d, 2*v, h-d-d);
+        speed .coord = xywh(l+0*w+ 4*v, H-h+d, 1*v, h-d-d);
+        fast  .coord = xywh(l+0*w+ 5*v, H-h+d, 2*v, h-d-d);
+        mute  .coord = xywh(l+0*w+ 7*v, H-h+d, 2*v, h-d-d);
+        sort  .coord = xywh(l+0*w+10*v, H-h+d, 2*v, h-d-d);
+        shuff .coord = xywh(l+0*w+12*v, H-h+d, 2*v, h-d-d);
 
         play  .coord = xywh(r-5*w-4*v, H-h+d, 2*w, h-d-d);
         play.enabled = onetwo.selected.now == 0
         and not Conon.on.now;
 
-        conon .coord = xywh(r-3*w-3*v, H-h+d, 1*v, h-d-d);
-        Conon .coord = xywh(r-3*w-2*v, H-h+d, 1*v, h-d-d);
-        trans .coord = xywh(r-3*w-1*v, H-h+d, 1*v, h-d-d);
-        onetwo.coord = xywh(r-3*w-0*v, H-h+d, 2*w, h-d-d);
-        dicon .coord = xywh(r-1*w-0*v, H-h+d, 1*w, h-d-d);
-        where .coord = xywh(r+0*w+1*v, H-h+d, 9*w, h-d-d);
+        conon .coord = xywh(r-3*w- 3*v, H-h+d, 1*v, h-d-d);
+        Conon .coord = xywh(r-3*w- 2*v, H-h+d, 1*v, h-d-d);
+        trans .coord = xywh(r-3*w- 1*v, H-h+d, 1*v, h-d-d);
+        onetwo.coord = xywh(r-3*w- 0*v, H-h+d, 2*w, h-d-d);
+        dicon .coord = xywh(r-1*w- 0*v, H-h+d, 1*w, h-d-d);
+        where .coord = xywh(r+0*w+ 1*v, H-h+d, 9*w, h-d-d);
 
         if (not conon.on.now) l = 0;
         if (not dicon.on.now) r = W;
@@ -260,6 +272,11 @@ widget<App>
             sys::settings::save("app::mute",
             mute.on? 1:0);
         }
+
+        if (what == &sort ) app::one::course.root.sort();
+        if (what == &shuff) app::one::course.root.shuffle();
+        if (what == &sort ) one_reload();
+        if (what == &shuff) one_reload();
     }
 
     bool mouse_sensible (xy) override { return true; }
